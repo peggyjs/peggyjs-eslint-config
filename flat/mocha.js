5d12c40abcecb3234796ff4614992036ac0a979c
Joe Hildebrand <joe-github@cursive.net>
2024-02-15 14:21:38 -0700

    Add mocha eslint plugin to the mocha config

--- flat/mocha.js
+++ flat/mocha.js
@@ -1,10 +1,18 @@
 "use strict";
 
 const globals = require("globals");
+const mocha = require("eslint-plugin-mocha");
 
 module.exports = {
   files: ["test/**"],
   languageOptions: {
     globals: globals.mocha,
+  },
+  plugins: {
+    mocha,
+  },
+  rules: {
+    ...mocha.configs.recommended.rules,
+    "mocha/no-mocha-arrows": "off",
   },
 };
--- test/flat/eslint.config.js
+++ test/flat/eslint.config.js
@@ -3,4 +3,5 @@
 module.exports = [
   require("@peggyjs/eslint-config/flat/js"),
   require("@peggyjs/eslint-config/flat/ts"),
+  require("@peggyjs/eslint-config/flat/mocha"),
 ];
