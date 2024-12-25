07fc92264df7e12828a8addd60e2024a817e6daa
Joe Hildebrand <joe-github@cursive.net>
2024-12-25 09:55:11 -0700

    Update dependencies

--- rules/json.js
+++ rules/json.js
@@ -2,4 +2,7 @@ export const rules = {
   // [Rules](https://github.com/eslint/json?tab=readme-ov-file#rules)
   "json/no-duplicate-keys": "error",
   "json/no-empty-keys": "error",
+  "json/no-unnormalized-keys": ["error", { form: "NFD" }],
+  "json/no-unsafe-values": "error",
+  "json/top-level-interop": "off",
 };
